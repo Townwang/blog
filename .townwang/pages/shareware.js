9a7f17e3c8f312abc10422740d66773d55a11dc3
Townwang <android@townwang.com>
2026-01-04 21:38:31 +0800

    Update shareware.js

--- .townwang/pages/shareware.js
+++ .townwang/pages/shareware.js
@@ -4,5 +4,11 @@ export const datas = [
   description:"this app description",
   version:"v1.0.0",
   downloadUrl:""
+ }, 
+ {
+  name:"test",
+  description:"this app description",
+  version:"v1.0.0",
+  downloadUrl:""
  }
 ]
