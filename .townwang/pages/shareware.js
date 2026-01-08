3bff6b960620af870bcc31361a2627e8bfb02281
Townwang <android@townwang.com>
2026-01-08 16:57:12 +0800

    更新 shareware.js

--- .townwang/pages/shareware.js
+++ .townwang/pages/shareware.js
@@ -7,10 +7,13 @@ export const datas = [
     version: "v4.4.4"
   },
   {
-    title: "测试名称",
-    desc: "测试描述",
-    link: "",
+    title: "Stratum",
+    desc: "Stratum 是一款开源的双因素认证应用程序，适用于 Android 系统。 它的特点是加密备份、图标、分类和高度定制化 它也有一个适用于 Wear OS 的应用
+
+它支持使用 SHA1、SHA256 或 SHA512 散列算法的 TOTP(基于时间) 和 HOTP(基于计数) 认证器 还支持移动Otp(mOTP)、Steam 和 Yandex 还支持移动Otp(mOTP)、Steam 和 Yandex
+
+二步验证要求使用一次性代码登录，从而为您的帐户提供额外的一层安全保护。 除了您的用户名和密码，您还需要 Stratum 提供的代码。 因此，即使您的登录信息被泄露，您的账户仍然是安全的。 因此，即使您的登录信息被泄露，您的账户仍然是安全的。",            link:"https://apt.izzysoft.de/fdroid/repo/com.stratumauth.app_12.apk",
     btnText: "立即下载",
-    version: "v1.0.0"
+    version: "v1.4.0"
   }
 ];
