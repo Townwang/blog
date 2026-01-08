985d7967da695b0744d281a932314724281c3901
Townwang <android@townwang.com>
2026-01-09 01:18:48 +0800

    更新 shareware.js

--- .townwang/pages/shareware.js
+++ .townwang/pages/shareware.js
@@ -1,14 +1,14 @@
 export const datas = [
   {
     title: "全球网测",
-    desc: "全球网测是中国信息通信研究院自主研发的专业网络测试工具，具备宽带测速、上网体验和网络诊断三大核心功能。该应用突出5G/千兆接入测速、宽带速率评价反馈、用户上网感知测评等六大技术优势，基于覆盖全国的千余台自有测量服务器，可满足专业技术人员与普通用户的多样化需求。以绿色免费、后台零自启的特点保障用户权益",
+    desc: "全球网测是中国信息通信研究院自主研发的专业网络测试工具，具备宽带测速、上网体验和网络诊断三大核心功能。",
     link: "https://down.cnspeedtest.cn:8043/TaierAndroid/update/globalspeed_4.4.4_safe.apk",
     btnText: "立即下载",
     version: "v4.4.4"
   },
   {
     title: "Stratum",
-    desc: "Stratum 是一款开源的双因素认证应用程序，适用于 Android 系统。 它的特点是加密备份、图标、分类和高度定制化 它也有一个适用于 Wear OS 的应用，它支持使用 SHA1、SHA256 或 SHA512 散列算法的 TOTP(基于时间) 和 HOTP(基于计数) 认证器 还支持移动Otp(mOTP)、Steam 和 Yandex 还支持移动Otp(mOTP)、Steam 和 Yandex，二步验证要求使用一次性代码登录，从而为您的帐户提供额外的一层安全保护。 除了您的用户名和密码，您还需要 Stratum 提供的代码。 因此，即使您的登录信息被泄露，您的账户仍然是安全的。 因此，即使您的登录信息被泄露，您的账户仍然是安全的。",            link:"https://apt.izzysoft.de/fdroid/repo/com.stratumauth.app_12.apk",
+    desc: "Stratum 是一款开源的双因素认证应用程序。",            link:"https://apt.izzysoft.de/fdroid/repo/com.stratumauth.app_12.apk",
     btnText: "立即下载",
     version: "v1.4.0"
   }
