b3d44c08db1073cb90598840bf22085079a4afe9
Townwang <android@townwang.com>
2026-01-21 00:31:14 +0800

    更新 index.ts

--- .townwang/.vitepress/theme/index.ts
+++ .townwang/.vitepress/theme/index.ts
@@ -2,10 +2,12 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import Download from './layouts/download.vue'
+import DailyPassword from './layouts/DailyPassword.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({app}) {
     app.component('download' , Download)
+    app.component('password' , DailyPassword)
   }
 }
