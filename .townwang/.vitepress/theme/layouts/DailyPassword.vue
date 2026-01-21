61efdbf2f1c77bd326cb898285974772cf0a97ad
Townwang <android@townwang.com>
2026-01-22 00:17:16 +0800

    更新 DailyPassword.vue

--- .townwang/.vitepress/theme/layouts/DailyPassword.vue
+++ .townwang/.vitepress/theme/layouts/DailyPassword.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="daily-password">
     <p>今日动态密码（{{ today }}）：<span class="password">{{ dailyPwd }}</span></p>
+    <div class="pwd-link-wrap">
+      <a href="https://townwang.com/pages/shareware" target="_blank" rel="noopener noreferrer">点击前往下载基地</a>
+    </div>
   </div>
 <Content />
 </template>
@@ -10,13 +13,25 @@ import { ref, onMounted, getCurrentInstance } from 'vue'
 const today = ref('')
 const dailyPwd = ref('')
 
-// 获取Vue全局实例，拿到挂载的$dailyPwd工具函数
 const { $dailyPwd } = getCurrentInstance().appContext.config.globalProperties
 
 onMounted(() => {
-  // 调用全局工具函数，获取日期和密码（可传参改长度，如$dailyPwd.getDateAndPwd(8)）
   const { today: t, dailyPwd: p } = $dailyPwd.getDateAndPwd()
   today.value = t
   dailyPwd.value = p
 })
 </script>
+
+<style scoped>
+.pwd-link-wrap {
+  text-align: center;
+  margin-top: 16px;
+}
+.pwd-link-wrap a {
+  color: #42b983;
+  text-decoration: none;
+}
+.pwd-link-wrap a:hover {
+  text-decoration: underline;
+}
+</style>
